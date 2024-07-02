d7d7d19c8515e315f3757a054e5493bb36444114
unknown <jagadeeshganta7@gmail.com>
2024-07-02 08:16:00 +0530

    counter app

--- src/App.js
+++ src/App.js
@@ -1,18 +1,21 @@
 import React from 'react';
 // import ParentComponent from './ParentComponent';
+import Counter from './Counter';
 // import Hotel from './Hotel';
-import photosData from './PhotosData';
-import Photos from './Photos';
+// import photosData from './PhotosData';
+// import Photos from './Photos';
 
 const App = () => {
   return (
     <div>
-      {photosData.map((eachObj) => {
+      {/* {photosData.map((eachObj) => {
         const { id, thumbnailUrl, title, url } = eachObj;
         return (
           <Photos id={id} title={title} thumbnailUrl={thumbnailUrl} url={url} />
         );
-      })}
+      })} */}
+      {/* <ParentComponent /> */}
+      <Counter />
     </div>
   );
 };
--- src/ChildComponent.js
+++ src/ChildComponent.js
@@ -1,13 +1,20 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 const ChildComponent = (props) => {
+  const { sendAge, sendName } = props;
   console.log(props);
   return (
     <div>
-      <div>Name:{props.sendName} </div>
-      <div>Age:{props.sendAge}</div>
+      <div>Name:{sendName} </div>
+      <div>Age:{sendAge}</div>
     </div>
   );
+};
+
+ChildComponent.propTypes = {
+  sendName: PropTypes.any,
+  sendAge: PropTypes.any,
 };
 
 export default ChildComponent;
